83c0b82d52067b36c6f905739fda0e39ee916a11
Ranadeep Mahendra <145512318+RanadeepMahendra2000@users.noreply.github.com>
2025-07-12 15:35:16 -0400

    Update leetcode_stats.py

--- leetcode_stats.py
+++ leetcode_stats.py
@@ -5,42 +5,42 @@ LEETCODE_USERNAME = 'ranadeep_mahendra2426'
 
 def fetch_leetcode_data(username):
     url = f'https://leetcode-stats-api.herokuapp.com/{username}'
-    r = requests.get(url)
-    r.raise_for_status()
-    return r.json()
+    resp = requests.get(url)
+    resp.raise_for_status()
+    return resp.json()
 
 def update_readme(data):
-    ts = int(time.time())  # ← generate timestamp once per run
+    ts = int(time.time())
 
     with open('README.md', 'w') as f:
         f.write("# 👋 Hi, I'm Ranadeep Mahendra!\n\n")
         f.write("## 🏆 My LeetCode Progress\n\n")
 
-        # ← Replace your old hard-coded badges with this dynamic block:
+        # dynamic, cache-busted badges:
         f.write(
-            f"![Solved]"
+            f"![LeetCode Questions Solved]"
             f"(https://img.shields.io/badge/Solved-{data['totalSolved']}/{data['totalQuestions']}-blue"
             f"?cache={ts}) "
         )
         f.write(
-            f"![Easy]"
+            f"![LeetCode Easy]"
             f"(https://img.shields.io/badge/Easy-{data['easySolved']}/{data['totalEasy']}-brightgreen"
             f"?cache={ts}) "
         )
         f.write(
-            f"![Medium]"
+            f"![LeetCode Medium]"
             f"(https://img.shields.io/badge/Medium-{data['mediumSolved']}/{data['totalMedium']}-orange"
             f"?cache={ts}) "
         )
         f.write(
-            f"![Hard]"
+            f"![LeetCode Hard]"
             f"(https://img.shields.io/badge/Hard-{data['hardSolved']}/{data['totalHard']}-red"
             f"?cache={ts})\n\n"
         )
 
         f.write("### 📊 LeetCode Activity Graph\n\n")
         f.write(
-            f"![Activity Graph]"
+            f"![LeetCode Activity Graph]"
             f"(https://leetcard.jacoblin.cool/{LEETCODE_USERNAME}"
             f"?theme=dark&font=Karma&ext=heatmap&cache={ts})\n"
         )
